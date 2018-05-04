c6824a81ba9e756eb107cce352e6563bdc4b5e20
Vivian <vhe1@andrew.cmu.edu>
2018-05-03 20:13:08 -0400

    heh

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
 	belongs_to :category
 
 	  validates_presence_of :title, :content
-
-	  scope :alphabetical, -> { order('title') }
 	  scope :active,       -> { where(active: true) }
+	  scope :alphabetical, -> { order('title') }
+	  
 end
