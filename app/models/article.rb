46109f923b467abeafd0265d8797a20c04a6ba30
Vivian <vhe1@andrew.cmu.edu>
2018-05-03 20:02:56 -0400

    added basic rel to article

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+	belongs_to :category
+
+	  validates_presence_of :title, :content
+
+	  scope :alphabetical, -> { order('title') }
+	  scope :active,       -> { where(active: true) }
+end
